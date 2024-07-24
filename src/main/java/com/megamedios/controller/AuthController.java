440225baa2a9adb997567bd8a3938a2fbc441b37
NanoBlanco <reynaldojblancom@gmail.com>
2024-07-24 17:33:09 -0400

    Creados jwtUtils, request, authresponse y estructura controladores

---
+++ src/main/java/com/megamedios/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.megamedios.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.megamedios.request.LoginRequest;
+import com.megamedios.response.AuthResponse;
+import com.megamedios.security.jwt.JwtUtils;
+import com.megamedios.service.UserService;
+import com.megamedios.entities.User;
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@CrossOrigin(origins="*", maxAge=3600)
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+	
+	private final UserService userService;
+	private final AuthenticationManager authenticationManager;
+	private final JwtUtils jwtUtils;
+	
+	@PostMapping("/login")
+	public ResponseEntity<AuthResponse> authenticateUser(@Valid @RequestBody LoginRequest request) {
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(request.getCorreo(), request.getClave()));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		String jwt = jwtUtils.generateJwtTokenForUser(authentication);
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		
+		String role = userDetails.getAuthorities().toString();
+		
+		AuthResponse authResponse = new AuthResponse(userDetails.getUsername(), jwt, role);
+		
+		return ResponseEntity.ok(authResponse);
+	}
+
+}
---
+++ src/main/java/com/megamedios/response/AuthResponse.java
@@ -0,0 +1,22 @@
+package com.megamedios.response;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+	
+	@NotBlank
+	private String correo;
+	@NotBlank
+	private String clave;
+	@NotBlank
+	private String role;
+
+}
