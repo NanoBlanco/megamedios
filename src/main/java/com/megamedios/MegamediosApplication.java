691869c3af3e44d8b999713726d779cef1ff2b4a
NanoBlanco <reynaldojblancom@gmail.com>
2024-07-24 15:38:39 -0400

    Commit inicial, app en cero

---
+++ src/main/java/com/megamedios/MegamediosApplication.java
@@ -0,0 +1,13 @@
+package com.megamedios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegamediosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegamediosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megamedios/MegamediosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megamedios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegamediosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
